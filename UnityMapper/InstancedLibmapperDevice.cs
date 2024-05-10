026e6a136579b309f4ed925fb5dec3aad25d3b5b
Kyle Smith <kyleafmine@gmail.com>
2024-05-10 15:16:12 -0300

    Add InstancedLibmapperDevice

---
+++ UnityMapper/InstancedLibmapperDevice.cs
@@ -0,0 +1,15 @@
+namespace UnityMapper;
+
+/// <summary>
+/// A version of <see cref="BaseLibmapperDevice"/> designed to be controlled by a separate component on a
+/// parent GameObject.
+///
+/// This device doesn't do anything by itself.
+/// </summary>
+public class InstancedLibmapperDevice : BaseLibmapperDevice
+{
+    void FixedUpdate()
+    {
+        
+    }
+}
