24ec57fae8e58910c9857a96b396b33412b5b14f
Kyle Smith <kyleafmine@gmail.com>
2024-05-13 13:44:25 -0300

    Refactor default property extraction logic to seperate class

--- UnityMapper/API/PropertyExtractor.cs
+++ UnityMapper/API/PropertyExtractor.cs
@@ -32,3 +32,36 @@ public interface IPropertyExtractor
 {
     List<IMappedProperty> ExtractProperties(Component component);
 }
+
+/// <summary>
+/// Reflection-based property extractor that maps fields to libmapper signals.
+/// </summary>
+/// <param name="_converters">Available type converters to primitivize types</param>
+public class DefaultPropertyExtractor(Dictionary<Type, ITypeConverter> _converters) : IPropertyExtractor
+{
+    public List<IMappedProperty> ExtractProperties(Component target)
+    {
+        var candidates = target.GetType().GetFields();
+        Debug.Log("Extracting properties from " + target.GetType());
+        var l = new System.Collections.Generic.List<IMappedProperty>();
+        foreach (var prop in candidates)
+        {
+            var baseType = LibmapperDevice.CreateLibmapperTypeFromPrimitive(prop.FieldType);
+            if (baseType == Mapper.Type.Null && !_converters.ContainsKey(prop.FieldType)) continue;
+            Debug.Log("Mapping property: " + prop.Name + " of type: " + baseType + " for libmapper.");
+            var mapped = new MappedClassField(prop, target);
+                
+            if (baseType == Mapper.Type.Null) // this type needs to be wrapped in order to be turned into a signal
+            {
+                var mapper = _converters[prop.FieldType];
+                l.Add(new WrappedMappedProperty(mapped, mapper));
+            }
+            else
+            {
+                l.Add(mapped);
+            }
+        }
+
+        return l;
+    }
+}
