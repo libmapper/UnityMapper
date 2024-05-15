6bd375490b32a4e61eb1a524f6c6579f833ffdb4
Kyle Smith <kyleafmine@gmail.com>
2024-05-15 12:14:09 -0300

    Fix comments and add constructor to SignalSpec

--- UnityMapper/Instances/SignalCollection.cs
+++ UnityMapper/Instances/SignalCollection.cs
@@ -19,29 +19,29 @@ public class SignalCollection
     }
     
     /// <summary>
-    /// A full path name to the signal, from the highest root G ameObject.
+    /// A full path name to the signal, from the highest root GameObject.
     /// </summary>
     public string Name { get; private set; }
     
 }
 
 /// <summary>
-/// Contains uniquely identifying information about a signal. Use 
+/// Contains uniquely identifying information about a signal. Used by a <see cref="SignalCollection"/> to group signals.
 /// </summary>
-public record SignalSpec
+public record SignalSpec(string LocalName, GameObject Owner, IBoundProperty Property)
 {
     /// <summary>
     /// The name of this signal, relative to it's owning object. For example, a camera's FOV slider would be "Camera/fov"
     /// </summary>
-    public string LocalName { get; private set; }
-    
+    public string LocalName { get; private set; } = LocalName;
+
     /// <summary>
     /// The GameObject that this property belongs to.
     /// </summary>
-    public GameObject Owner { get; private set; }
-    
+    public GameObject Owner { get; private set; } = Owner;
+
     /// <summary>
     /// Accessor for the property on that specific object
     /// </summary>
-    public IBoundProperty Property { get; private set; }
+    public IBoundProperty Property { get; private set; } = Property;
 }
